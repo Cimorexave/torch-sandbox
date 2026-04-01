c89e842d5c4083d5ece398c9dcaec0b39ea8f9b6
Sina Sadeghi <sadeq.s502@gmail.com>
2026-04-01 17:29:57 +0200

    tensor properties

--- python/tensor-1.py
+++ python/tensor-1.py
@@ -4,5 +4,24 @@ from torch import Tensor
 
 data = [[1,2,3,], [4,5,6]]
 t_data = torch.tensor(data)
-print(t_data)
-print(t_data.dtype)
+# print(t_data)
+# print(t_data.dtype)
+
+shape = (2,3)
+# rand_tensor = torch.rand(shape)
+# print(rand_tensor)
+ones = torch.ones(shape)
+zeros = torch.zeros(shape)
+# print(zeros)
+# print(ones)
+
+likeness_zero = torch.rand_like(zeros, dtype=zeros.dtype)
+# print(f"templaete zeros: {zeros}, \nlikeness zeros: {likeness_zero}")
+
+# properties
+tensor = torch.rand(3,4)
+# print(f"shape: {tensor.shape}, \ndtype: {tensor.dtype}, \ndevice: {tensor.device}")
+
+t_data = torch.tensor(data, dtype=torch.int16)
+param_data = torch.tensor(data, dtype=torch.float32, device="cpu", requires_grad=True)
+
