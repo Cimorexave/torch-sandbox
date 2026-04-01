5a5ec26895b48e478d090fcc220a69ebeed7b560
Sina Sadeghi <sadeq.s502@gmail.com>
2026-04-01 17:51:12 +0200

    argmax

--- python/operations.py
+++ python/operations.py
@@ -23,3 +23,12 @@ avg_per_assignment = scores.mean(dim=0)
 print(f"avg_per_student: {avg_per_student}, \navg_per_assignment: {avg_per_assignment}")
 # dimension 0 collapses vertically (does operation per column, like mean per column), 
 # dimension 1 collapses horizontally(does operation per row, like mean per row), 
+
+
+scores = tensor([
+    [10, 0 , 5 , 20, 1], 
+    [1, 30 , 2, 5 ,0]
+], dtype=torch.float32)
+best_indices = scores.argmax(dim=1)
+print(f"best_indices: {best_indices}")
+# gives you the position of the max value in each row, so for the first row it gives 3 because 20 is the max value and it's in position 3, for the second row it gives 1 because 30 is the max value and it's in position 1. result: tensor([3, 1])
